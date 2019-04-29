422fe8fad3b0a685ace92acde4d92171282c5b0b
dong <dong@localhost.localdomain>
2019-04-28 21:01:20 -0400

    1.5 Simple web responses with Laravel

--- landon_app/routes/web.php
+++ landon_app/routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h3>Landon App Page</h3>';
+    // return view('welcome');
+});
+
+Route::get('/about', function () {
+    $response_arr = [];
+    $response_arr['author'] = 'BP';
+    $response_arr['version'] = '0.1.1';
+    return $response_arr;
+    // return '<h3>About</h3>';
+});
+
+Route::get('/home', function () {
+    $data = [];
+    $data['version'] = '0.1.1';
+    return view('welcome', $data);
 });
